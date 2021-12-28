91cbb2a34d630a9ee45ee8b13e93450a2c7b1cf1
Irina Sabaliauskiene <sabaliauskieneirina@gmail.com>
2021-12-28 23:05:45 +0100

    video done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,14 @@
-const refs = {
-  ourvideo: document.querySelector('#vimeo-player'),
-};
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+  }, 1000),
+);
+const data = localStorage.getItem('videoplayer-current-time');
+console.log(data);
+player.setCurrentTime(data);
